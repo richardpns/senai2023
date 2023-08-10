3cae97be95afe5770796f2303df5679be77bb717
richardpns <140808523+richardpns@users.noreply.github.com>
2023-08-10 19:05:10 -0300

    Update desafio06.cpp

--- 1des/FPOO/lista01/desafio06.cpp
+++ 1des/FPOO/lista01/desafio06.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <iostream>
 int main(){
   int vel = 900 ;
   int dis;
   //entradas
-  printf ("a velocidade de cruzeiro do 747-300 e de 900km/h")
+  std::cout << "a velocidade de cruzeiro do 747-300 e de 900.0km/h" << std::endl;
   printf ("digite a distancia a ser percorrida");
   scanf ("%d" ,&dis);
   //processamento 
@@ -11,6 +12,6 @@ int main(){
   //saidas
   printf("velocidade = %d\n",vel);
   printf("distancia = %d\n",dis);
-  printf("o tempo a ser percorrido é = %d\n",result);
+  printf("o tempo a ser percorrido Ã© = %d\n",result);
   return 0;
 }
